99fc97e1f05224b27fb92c7ed4f17f98cd1c5414
rachaelkiteme <rachaelrirrie@gmail.com>
2017-09-09 19:46:47 +0300

    puts that is printing

---
+++ hello.rb
@@ -0,0 +1,20 @@
+
+#print hello world
+# single line comment
+puts "hello world"
+=begin 
+mutiple line comments
+=end
+# data types
+puts "a doble quotes"
+#interpolation
+age = 20
+puts "My age is #{age}"
+puts 'My age is #{age}'
+puts 'single quote'
+#string methods
+puts "firstname","secondname" #concatenation
+puts "hi" *3
+puts "string".upcase
+puts "mystring".length
+puts "mystring".reverse
