c2e1d9300d49a13162aee2c91f41b94518a21e8b
rachaelkiteme <rachaelrirrie@gmail.com>
2017-09-09 20:15:39 +0300

    add arrays

---
+++ arrays.rb
@@ -0,0 +1,16 @@
+#craeting list
+rgnb = ["rachael","natasha","stella",3]
+rgnb << "cecilia"
+p rgnb
+#setting values
+#arrays begin at 0
+rgnb[1] = "wilma"
+p rgnb
+#other array method
+rgkla = ["Keziah","clare"]
+p rgnb + rgkla
+p rgnb.first
+p rgnb.last
+p rgnb.length
+p rgnb.index("stella")
+p rgnb.sort
