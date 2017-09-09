9eb4bc6402f50aa671115ebbd4bc9ba7caa7e29d
rachaelkiteme <rachaelrirrie@gmail.com>
2017-09-09 20:34:32 +0300

    add hash

---
+++ harsh.rb
@@ -0,0 +1,12 @@
+dict = {"name"=> "rachel", "school" => "KU", "age" => 85}
+dict2 = {"name"=> "natasha", "school" => "USIU", "age" => 20}
+p dict["name"]
+p dict["age"]
+p dict["age"]=16
+p dict
+#other methods
+
+p dict.keys
+p dict.values
+p dict.length
+p dict.merge(dict2)
